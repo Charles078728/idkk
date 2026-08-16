548ea95816a5e15758426b9d07c0c2d365447206
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2026-08-16 21:53:56 +0800

    Add A/B OTA tools and F2FS tools to device.mk

--- device.mk
+++ device.mk
@@ -1,1 +1,19 @@
 
+# Explicitly pull in A/B OTA tools
+AB_OTA_UPDATER := true
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6761 \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-impl.recovery
+
+# F2FS tools
+PRODUCT_PACKAGES += \
+    sg_write_buffer \
+    f2fs_io \
+    check_f2fs
+
+# fastbootd (for dynamic partition flashing from recovery)
+PRODUCT_PACKAGES += \
+    fastbootd
