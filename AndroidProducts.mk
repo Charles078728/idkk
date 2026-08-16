823630b497c065353d575d6689ed59238d5d9847
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2026-08-16 16:08:39 +0800

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_BF7.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_BF7-user \
+    twrp_BF7-userdebug \
+    twrp_BF7-eng
