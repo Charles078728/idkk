fa1cfe93db22fa97d53d9998c28eb87847cc717e
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2026-08-17 19:16:35 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,10 +3,15 @@
 AB_OTA_UPDATER := true
 
 PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1-service.trustonic \
     bootctrl.mt6761 \
+    bootctrl.mt6761.recovery \
     android.hardware.boot@1.2-impl \
     android.hardware.boot@1.2-service \
-    android.hardware.boot@1.2-impl.recovery
+    android.hardware.boot@1.2-impl.recovery \
+    vendor.trustonic.tee@1.1-service \
+    vendor.mediatek.hardware.keymaster_attestation@1.1-service
+
 
 # F2FS tools
 PRODUCT_PACKAGES += \
